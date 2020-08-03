b62940d406a60e54d673f6327cb604d4dd02b1a2
JuanpaBP <juanpaulich2020@gmail.com>
2020-08-03 16:12:49 -0300

    Initial Commmit

---
+++ src/test/java/com/github/juanpabp/spring/FacturacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.juanpabp.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacturacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
